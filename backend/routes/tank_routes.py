10dd3da5c07bb8e9a8249c44bfeb0202ae1e16ca
apaolol <bautrokyle@gmail.com>
2026-07-22 17:07:03 +0800

    done tanks?

--- backend/routes/tank_routes.py
+++ backend/routes/tank_routes.py
@@ -1,71 +1,37 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from models.tank_profile import TankProfile
+import services.tank_services as service
+from sqlmodel import Session
 from database.db import get_session
-from sqlmodel import Session, select
 
 
 router = APIRouter()
 
 # create tank profike
-@router.post("/")
+@router.post("/", response_model=TankProfile)
 def create_tank(tank_profile: TankProfile, db: Session = Depends(get_session)):
-    # Logic to create a new tank profile in
-    tank_profile.capacity = tank_profile.volume_ml * 600
-
-    db.add(tank_profile)
-    db.commit()
-    #db.refresh(tank_profile)
-    return {"message": "Tank Profile Successfully  Added"}
+    return service.create_tank(tank_profile=tank_profile, db=db)
 
 # get all tanks
 @router.get("/", response_model=list[TankProfile])
 def get_all_tanks(
-    skip: int = 0, limit: int = 10, db: Session = Depends(get_session)
-    ):
-    # Logic to retrieve all tanks from the database
-    tanks = db.exec(
-        select(TankProfile)
-        .offset(skip)
-        .limit(limit)).all()
-    return tanks
+    skip: int = 0, limit: int = 10, db: Session = Depends(get_session)):
+    return service.get_all_tanks(skip=skip, limit=limit, db=db)
 
 # specific one tank onleh
 @router.get("/{tank_id}", response_model=TankProfile)
 def view_tank(tank_id: int, db: Session = Depends(get_session)):
-    # Logic to retrieve
-    tank = db.get(TankProfile, tank_id)
-    if not tank:
-        raise HTTPException(status_code=404, detail="Tank not found")
-    return tank
+    return service.view_tank(tank_id=tank_id, db=db)
 
 #update one tank
-@router.put("/{tank_id}")
+@router.put("/{tank_id}", response_model=TankProfile)
 def update_tank(
     tank_id: int, tank_data: TankProfile, db: Session = Depends(get_session)
     ):
-    # Logic to update an existing tank profile
-    tank = db.get(TankProfile, tank_id)
-    if not tank:
-        raise HTTPException(status_code=404, detail="Tank not Found")
-    
-    # update logic
-    for key, val in tank_data.model_dump(exclude={"id", "capacity"}).items():
-        setattr(tank, key, val)
 
-    tank.capacity = tank.volume_ml * 600
-
-    db.commit()
-    db.refresh(tank)
-
-    return {"message": "Tank updated successfully!"}
+    return service.update_tank(tank_id=tank_id, tank_data=tank_data, db=db)
 
 @router.delete("/{tank_id}")
 def delete_tank(tank_id: int, db: Session = Depends(get_session)):
-    # Logic to delete a tank profile
-    tank = db.get(TankProfile, tank_id)
-    if not tank:
-        raise HTTPException(status_code=404, detail="Tank not Found")
-    db.delete(tank)
-    db.commit()
 
-    return {"message": "Tank deleted successfully!"}
+    return service.delete_tank(tank_id=tank_id, db=db)
---
+++ backend/services/tank_services.py
@@ -0,0 +1,62 @@
+from fastapi import HTTPException
+from models.tank_profile import TankProfile
+from sqlmodel import Session, select
+
+# create tank profike
+def create_tank(tank_profile: TankProfile, db: Session):
+    # Logic to create a new tank profile in
+    tank_profile.capacity = tank_profile.volume_ml * 600
+
+    db.add(tank_profile)
+    db.commit()
+    #db.refresh(tank_profile)
+    return {"message": "Tank Profile Successfully  Added"}
+
+# get all tanks
+def get_all_tanks(
+     db: Session, skip: int = 0, limit: int = 10):
+    # Logic to retrieve all tanks from the database
+    tanks = db.exec(
+        select(TankProfile)
+        .offset(skip)
+        .limit(limit)).all()
+    return tanks
+
+# specific one tank onleh
+def view_tank(tank_id: int, db: Session):
+    # Logic to retrieve
+    tank = db.get(TankProfile, tank_id)
+    if not tank:
+        raise HTTPException(status_code=404, detail="Tank not found")
+    return tank
+
+#update one tank
+def update_tank(
+    tank_id: int, tank_data: TankProfile, db: Session
+    ):
+    # Logic to update an existing tank profile
+    tank = db.get(TankProfile, tank_id)
+    if not tank:
+        raise HTTPException(status_code=404, detail="Tank not Found")
+    
+    # update logic
+    for key, val in tank_data.model_dump(exclude={"id", "capacity"}).items():
+        setattr(tank, key, val)
+
+    tank.capacity = tank.volume_ml * 600
+
+    db.commit()
+    db.refresh(tank)
+
+    return {"message": "Tank updated successfully!"}
+
+
+def delete_tank(tank_id: int, db: Session):
+    # Logic to delete a tank profile
+    tank = db.get(TankProfile, tank_id)
+    if not tank:
+        raise HTTPException(status_code=404, detail="Tank not Found")
+    db.delete(tank)
+    db.commit()
+
+    return {"message": "Tank deleted successfully!"}
